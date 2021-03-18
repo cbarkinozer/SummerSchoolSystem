1d782c467d4bbd5d50afda3338ba3162187d6bf7
Barkin Ozer <c.barkinozer@gmail.com>
2021-03-18 15:22:44 +0300

    Add project files.

---
+++ EntityLayer/EntityLesson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EntityLesson
+{
+    public class EntityLesson
+    {
+        private int LessonId { get { return LessonId; } set { LessonId = value; } }
+        private string LessonName { get { return LessonName; } set { LessonName = value; } }
+        private int LessonQuota { get { return LessonQuota; } set { LessonQuota = value; } }
+
+
+    }
+}
