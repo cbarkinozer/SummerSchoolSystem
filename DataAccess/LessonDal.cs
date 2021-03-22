1d01aae85b295f342f1e6fad49149b500d7ba081
Barkin Ozer <c.barkinozer@gmail.com>
2021-03-23 01:25:53 +0300

    Lesson data access layer listing added

---
+++ DataAccess/LessonDal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entity;
+
+namespace DataAccess
+{
+   public  class LessonDal
+    {
+        public static List<EntityLesson> LessonList()
+        {
+            List<EntityLesson> values = new List<EntityLesson>();
+            SqlCommand command1 = new SqlCommand("Select * From TblLesson", Connection.con);
+            if (command1.Connection.State != ConnectionState.Open)
+            {
+                command1.Connection.Open();
+            }
+            SqlDataReader dataReader = command1.ExecuteReader();
+            while (dataReader.Read())
+            {
+                EntityLesson entityLesson = new EntityLesson();
+                entityLesson.LessonId = Convert.ToInt32(dataReader["LessonId"].ToString());
+                entityLesson.LessonName = dataReader["LessonName"].ToString();
+                entityLesson.LessonQuota = int.Parse(dataReader["LessonQuota"].ToString();); 
+                
+                values.Add(entityLesson);
+            }
+            dataReader.Close();
+            return values;
+        }
+    }
+}
